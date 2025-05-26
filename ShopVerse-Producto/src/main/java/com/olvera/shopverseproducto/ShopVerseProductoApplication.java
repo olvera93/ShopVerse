d7722870e1189448f36b2214fefe80291e062f0e
olvera93 <olvera9302@outlook.com>
2025-05-25 20:31:15 -0600

    [ADD] First Product microservice

---
+++ ShopVerse-Producto/src/main/java/com/olvera/shopverseproducto/ShopVerseProductoApplication.java
@@ -0,0 +1,13 @@
+package com.olvera.shopverseproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopVerseProductoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopVerseProductoApplication.class, args);
+    }
+
+}
